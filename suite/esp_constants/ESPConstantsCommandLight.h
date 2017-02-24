1d36d6a4ca11e5d4c543cfc4a55652f67c893cc5
3Reality_Zhen <zhenz@3reality.com>
2017-02-24 16:42:28 +0800

    iOS App init ver
    Initial check in the iOS app code

---
+++ suite/esp_constants/ESPConstantsCommandLight.h
@@ -0,0 +1,26 @@
+//
+//  ESPConstantsCommandLight.h
+//  suite
+//
+//  Created by 白 桦 on 6/8/16.
+//  Copyright © 2016 白 桦. All rights reserved.
+//
+
+#ifndef ESPConstantsCommandLight_h
+#define ESPConstantsCommandLight_h
+
+#define ESP_LIGHT_VERSION_NEW_PROTOCOL      @"v1.3.0t45772(o)"
+#define ESP_LIGHT_PERIOD_DEFAULT            1000
+
+#define KEY_STATUS      @"status"
+#define KEY_PERIOD      @"period"
+#define KEY_RED         @"red"
+#define KEY_GREEN       @"green"
+#define KEY_BLUE        @"blue"
+#define KEY_WHITE       @"white"
+#define KEY_COLOR       @"color"
+#define KEY_RGB         @"rgb"
+#define KEY_CWHITE      @"cwhite"
+#define KEY_WWHITE      @"wwhite"
+
+#endif /* ESPConstantsCommandLight_h */
